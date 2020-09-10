fcdbac069377f4b5857e6bc439d04a64ea4106db
Murad Abbasov <Murad@mrs-er-10-133-58-117.wireless.umn.edu>
2020-09-10 01:34:47 -0500

    Update regex

--- regex/regex.sh
+++ regex/regex.sh
@@ -1,1 +1,15 @@
-:
+#!/usr/bin/env bash
+
+#!/bin/bash
+
+#Regex output 0
+
+sed -E 's/\* ([a-zA-Z]+), ([a-zA-Z]+)/1. \1\n2. \2\n/' < r0_input.txt > r0_output.txt
+
+#Regex output 1
+
+sed -En 's/\* I am ([a-zA-Z]+). My favorite sandwich is ([a-zA-Z]+)./1. \1\n2. \2\n/p' < r1_input.txt > r1_output.txt
+
+#Regex output 2
+
+sed -E 's/\* sandwich with ([a-zA-Z.]+) (for here|to go)/1. \1\n2. \2\n/' < r2_input.txt > r2_output.txt
